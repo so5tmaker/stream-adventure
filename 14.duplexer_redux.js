e21488da1a8339cf86fcb40bead9b4b1fc7182ce
Viktor Sivulskiy <info@softmaker.kz>
2023-06-13 08:06:06 +0600

    duplexer-redux

--- 14.duplexer_redux.js
+++ 14.duplexer_redux.js
@@ -1,46 +1,55 @@
-// 5b. Стримы для записи
-// -[] Прототайп конспект https://youtu.be/0vs6WkNyzec, https://youtu.be/ncaWoyLINoI
-
 // ## DUPLEXER REDUX (Exercise 14 of 17)  
-   
+
 //   In this example, you will be given a readable stream, counter, as the  
 //   first argument to your exported function:  
-   
+
 //      module.exports = function (counter) {  
 //        // return a duplex stream to count countries on the writable side  
 //        // and pass through `counter` on the readable side  
 //      }  
-   
+
 //   Return a duplex stream with the counter as the readable side. You will be  
 //   written objects with a 2-character country field as input, such as these:  
-   
+
 //        {"short":"OH","name":"Ohio","country":"US"}  
 //        {"name":"West Lothian","country":"GB","region":"Scotland"}  
 //        {"short":"NSW","name":"New South Wales","country":"AU"}  
-   
+
 //   Create an object to track the number of occurrences of each unique country  
 //   code.  
-   
+
 //   For example:  
-   
+
 //        {"US": 2, "GB": 3, "CN": 1}  
-   
+
 //   Once the input ends, call counter.setCounts() with your counts object.  
-   
+
 //   The duplexer2 module will again be very handy in this example.  
-   
+
 //   If you use duplexer, make sure to npm install duplexer2 in the directory  
 //   where your solution file is located.  
-   
+
 //   Keep in mind that you will have to work with objects, not buffers. Consult  
 //   the documentation for further details:  
 //   (https://nodejs.org/api/stream.html#stream_object_mode)  
-   
+
 //   When you switch on the object mode, remember to do the same for all  
 //   additional dependencies that you work with (i.e. through2)  
-   
+
 //   Create a new file called duplexer-redux.js which will hold your solution.  
-   
+
 //   To verify your solution run:  
-   
+
 //      $ stream-adventure verify duplexer-redux.js  
+
+const duplex = require('duplexer2');
+const through = require('through2');
+
+module.exports = function (counter) {
+    // return a duplex stream to count countries on the writable side  
+    // and pass through `counter` on the readable side
+
+    const thr = through({ objectMode: true }, (chunk, _, next) => {
+
+    })
+} 
